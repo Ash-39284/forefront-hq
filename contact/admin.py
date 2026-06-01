6f982f1ce6ca75376d2a329f0ab0cf9026c83970
Ash-39284 <ashroberts98@icloud.com>
2026-06-01 14:46:57 +0100

    Add admin views to the contact app

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Service, ContactEnquiry
 
-# Register your models here.
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'is_active', 'display_order']
+
+@admin.register(ContactEnquiry)
+class ContactEnquiryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'service', 'status', 'submitted_at']
+    list_filter = ['status']
