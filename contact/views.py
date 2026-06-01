6b1b30660210da5b53bfba11721ac4aa814b78d3
Ash-39284 <ashroberts98@icloud.com>
2026-06-01 14:55:18 +0100

    Add urls and views to contact app. Add links to footer links on base.html and added contact.html to contact app

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Service, ContactEnquiry
 
-# Create your views here.
+def contact(request):
+    services = Service.objects.filter(is_active=True)
+
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        service_id = request.POST.get('service')
+        message = request.POST.get('message')
+
+        service = None
+        if service_id:
+            try:
+                service = Service.objects.get(id=service_id)
+            except Service.DoesNotExist:
+                pass
+
+        ContactEnquiry.objects.create(
+            user=request.user if request.user.is_authenticated else None,
+            service=service,
+            name=name,
+            email=email,
+            message=message,
+        )
+
+        messages.success(request, 'Your message has been sent. We\'ll be in touch within 24 hours.')
+        return redirect('contact')
+
+    return render(request, 'contact/contact.html', {'services': services})
--- forefront_hq/urls.py
+++ forefront_hq/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('accounts.urls')),
     path('', include('packages.urls')),
+    path('', include('contact.urls')),
 ]
