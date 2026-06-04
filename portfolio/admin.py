889feb46aef68053740927013b5225f3fd2e1142
Ash-39284 <ashroberts98@icloud.com>
2026-06-04 22:55:30 +0100

    Add admin views for portfolio app

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import PortfolioProject, ProjectTag, PortfolioProjectTag
 
-# Register your models here.
+class PortfolioProjectTagInline(admin.TabularInline):
+    model = PortfolioProjectTag
+    extra = 1
+
+@admin.register(PortfolioProject)
+class PortfolioProjectAdmin(admin.ModelAdmin):
+    list_display = ['title', 'client_name', 'category', 'is_live', 'is_featured', 'completed_at']
+    inlines = [PortfolioProjectTagInline]
+
+@admin.register(ProjectTag)
+class ProjectTagAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
