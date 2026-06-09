40e09896d3a25fd0c37d77ce74da6db79535ee7c
Ash-39284 <ashroberts98@icloud.com>
2026-06-09 17:42:39 +0100

    Add about to admin views

--- about/admin.py
+++ about/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import StaffAbout
 
-# Register your models here.
+@admin.register(StaffAbout)
+class StaffAboutAdmin(admin.ModelAdmin):
+    list_display = ['name', 'profile_img', 'job_title', 'bio_description', 'display_order']
--- about/models.py
+++ about/models.py
@@ -1,2 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+class StaffAbout(models.Model):
+    name = models.CharField(max_length=20, blank=True)
+    profile_img = models.ImageField(default=False)
+    job_title = models.CharField(max_length=25, blank=True, null=True)
+    bio_description = models.TextField(max_length=255, blank=True, null=True)
+    is_staff = models.BooleanField(default=False)
+    display_order = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self
