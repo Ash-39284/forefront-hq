770d0813c09b6d9e3bb9aafdabd8175b1804f336
Ash-39284 <ashroberts98@icloud.com>
2026-07-18 00:09:34 +0100

    Change debug settings for railway deployment

--- forefront_hq/settings.py
+++ forefront_hq/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'forefront-hq-production-4972.up.railway.app']
 
