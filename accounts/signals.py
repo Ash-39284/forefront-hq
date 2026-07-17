967b1529fbd9bf54d0d8cd0cdb77bb95a3d6d029
Ash-39284 <ashroberts98@icloud.com>
2026-07-18 00:33:24 +0100

    Fix Postgres driver and loaddata signal handling

--- accounts/signals.py
+++ accounts/signals.py
@@ -4,10 +4,14 @@ from django.dispatch import receiver
 from .models import UserProfile
 
 @receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
+def create_user_profile(sender, instance, created, raw=False, **kwargs):
+    if raw:
+        return
     if created:
         UserProfile.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
+def save_user_profile(sender, instance, raw=False, **kwargs):
+    if raw:
+        return
     instance.profile.save()
