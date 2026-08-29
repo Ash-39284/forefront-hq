1832eac43eaf024a7bf90654f5f1b6b0c9854e9d
Ash-39284 <ashroberts98@icloud.com>
2026-08-29 12:27:20 +0100

    Update views for portfolio app and linked in portfolio.html

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,8 +3,8 @@ from .models import PortfolioProject, PortfolioUpcoming
 
 def portfolio(request):
     projects = PortfolioProject.objects.filter(is_live=True).prefetch_related('tags').order_by('-completed_at')
-    return render(request, 'portfolio/portfolio.html', {'projects': projects})
-
-def upcoming(request):
-    projects = PortfolioUpcoming.objects.filter(is_live=False).prefetch_related('tags').order_by('-completed_at')
-    return render(request, 'portfolio/upcoming.html', {'projects': projects})
+    upcoming_projects = PortfolioUpcoming.objects.prefetch_related('tags').order_by('-completed_at')
+    return render(request, 'portfolio/portfolio.html', {
+        'projects': projects,
+        'upcoming_projects': upcoming_projects,
+    })
